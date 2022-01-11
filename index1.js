c9e84118313c061ccdd608818127ceaf00d9d990
mithun139 <mithunchakravarthi139@gmail.com>
2022-01-11 11:40:23 +0530

    Added successfully

---
+++ index1.js
@@ -0,0 +1,105 @@
+//variables practice
+
+ // Create two variables, firstName and lastName
+// let firstName = "Per"
+// let lastName = "Borgen"
+// Concatenate the two variables into a third variable called fullName
+
+// let fullName = firstName + " " + lastName
+
+// Log fullName to the console
+// console.log(fullName)
+
+//Concatenate two strings in a function
+
+// let name = "Linda"
+// let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+// function greetLinda() {
+//     console.log(greeting + ", " + name + "!")
+// }
+
+// greetLinda()
+
+//Incrementing and decrementing
+
+// let myPoints = 3
+
+// Create two functions, add3Points() and remove1Point(), and have them
+// add/remove points to/from the myPoints variable
+
+// function add3Points() {
+//     myPoints += 3
+// }
+
+// function remove1Points() {
+//     myPoints -= 1
+// }
+
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Points()
+// remove1Points()
+
+// Call the functions to that the line below logs out 10
+// console.log(myPoints)
+
+//Strings and Numbers
+
+// Try to predict what each of the lines will log out
+// console.log("2" + 2) // "22" 
+// console.log(11 + 7) //  18
+// console.log(6 + "5") // "65"
+// console.log("My points: " + 5 + 9) // "My points: 59"
+// console.log(2 + 2) // 4
+// console.log("11" + "14") //  "1114"
+
+
+//Rendering an error message
+
+// let errorParagraph = document.getElementById("error")
+// console.log(errorParagraph)
+
+// function purchase() {
+//     console.log("button clicked")
+//     errorParagraph.textContent = "Something went wrong, please try again"
+// }
+
+//Calculator |Challenge
+
+// let num1 = 8
+// let num2 = 2
+// document.getElementById("num1-el").textContent = num1
+// document.getElementById("num2-el").textContent = num2
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+// let sumEl = document.getElementById("sum-el")
+
+// function add() {
+//     let result = num1 + num2
+//     sumEl.textContent = "Sum: " + result
+// }
+
+// function subtract() {
+//     let result = num1 - num2
+//     sumEl.textContent = "Sum: " + result
+// }
+
+// function divide() {
+//     let result = num1 / num2
+//     sumEl.textContent = "Sum: " + result
+// }
+
+// function multiply() {
+//     let result = num1 * num2
+//     sumEl.textContent = "Sum: " + result
+// }
